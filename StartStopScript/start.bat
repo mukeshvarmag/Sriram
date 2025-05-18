018c05aba16a1fc5462c393f26055f09af4fbf9b
Gugulothu Mukesh Varma <mukeshvarma.g@gmail.com>
2025-05-18 20:55:01 +0530

    Mukesh | cmds for start and stop in local

--- StartStopScript/start.bat
+++ StartStopScript/start.bat
@@ -6,3 +6,8 @@ timeout /t 5
 cd frontend
 start cmd /k npm run dev
 cd ..
+
+
+# In local development, you can run the following commands to start and stop the servers:
+# .\StartStopScript\start.bat
+# .\StartStopScript\stop.bat
